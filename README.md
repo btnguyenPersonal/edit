1b00c84aaf7ca025a0cb8df2ab642b3531565689
btnguyenPersonal <peteraxefan@gmail.com>
2023-02-23 19:54:38 -0600

    got file rendering first starts

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ this is my experiment for making a new code editor
     going to fork vscode and see if I can just remove the stuff I don't want
         - bruh that shit is so complicated
     start with just single window with move around, insert characters, delete, save
+    good progress first day, `terminal-kit` library is awesome, much easier than that library for rust apps
 
 ## todo
     - make cursor and scrolling
--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const term = require('terminal-kit').terminal;
+const fs = require('fs');
+let file;
+if (process.argv.length >= 3) {
+    file = process.argv[2];
+}
+let rawdata = file ? fs.readFileSync(file, 'utf-8') : 'default data';
+data = rawdata.split('\n');
 
 term.grabInput({ mouse: 'button' });
 term.fullscreen(true);
 term.windowTitle('rotide');
+term(data.join('\n'));
+term.moveTo(0, 0);
 
 term.on('key', (key, data) => {
     if (key === 'CTRL_C') {
