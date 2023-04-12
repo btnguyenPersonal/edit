4457cd853b1029ab63dbfd0f4c9cd0c89cba8ba1
btnguyenPersonal <peteraxefan@gmail.com>
2023-04-12 12:44:49 -0500

    screen buffer

--- src/edit.js
+++ src/edit.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import fs from 'fs';
 import pkg from 'terminal-kit';
-const { terminal } = pkg;
+const { terminal, ScreenBuffer } = pkg;
 import * as helper from './util/helper.js';
 import { handleKeys } from './keybinds/normalKeys.js';
 import { handleVimKeys } from './keybinds/vimKeys.js';
@@ -43,20 +43,28 @@ term.grabInput({ mouse: 'button' });
 term.fullscreen(true);
 term.windowTitle('edit');
 
-helper.renderScreen(state, term);
+var screen = new ScreenBuffer( { dst: term , noFill: true } ) ;
+
+// screen.put({
+//     x: 1,
+//     y: y
+// }, 'hello');
+// screen.draw({ delta: true });
+
+helper.renderScreen(state, screen);
 
 term.on('key', (key) => {
     if (state.vim && state.mode !== 'i') {
-        handleVimKeys(key, state, term);
+        handleVimKeys(key, state, screen, term);
     } else {
-        handleKeys(key, state, term);
+        handleKeys(key, state, screen, term);
     }
 });
 
 term.on('mouse', (name, coor) => {
-    handleMouseInputs(name, coor, state, term);
+    handleMouseInputs(name, coor, state, screen);
 });
 
 term.on('resize', () => {
-    helper.renderScreen(state, term);
+    helper.renderScreen(state, screen);
 });
