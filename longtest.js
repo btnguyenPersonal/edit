4e11574af4d6c233e34aa0a8d662ef743453501e
btnguyenPersonal <btnguyenPersonal@gmail.com>
2023-05-18 19:13:15 -0500

    sometime will add merge conflict highlighting

--- longtest.js
+++ longtest.js
@@ -13,25 +13,16 @@ function whatever() {
     testing();
 }
 
-function torvalds({ state }) {
-    return 0;
-}#!/bin/hello
+<<<<<<< HEAD
+-Stacy
+-Alexander
 
-let hello = "testi';asldkf;asdfa'sdf'asdn{//g"'';  //
-let url = "http://www.\"google.com"; // how are you doing?
-function testing() {
-    let forever;
-    for (let i = 0; i < state.data.length; i += 1) {
-        if (state.row === 5) {
-            break;
-        }
-    }
-}
-
-function whatever() {
-    testing();
-}
-
-function torvalds({ state }) {
-    return 0;
-}
+Marketing team
+- Collins
+- Linda
+- Patricia
+- Morgan
+- Amanda
+=======
+testing
+>>>>>>> master
