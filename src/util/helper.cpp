1dc837bccc66cc87a20ea1e6b3ae8f66f816cd58
btnguyenPersonal <peteraxefan@gmail.com>
2024-01-09 06:30:40 -0600

    fixed file reading newline at end problem

--- src/util/helper.cpp
+++ src/util/helper.cpp
@@ -19,7 +19,8 @@ std::vector<std::string> readFile(std::string filename) {
     std::ifstream file(filename);
     std::string str;
     std::vector<std::string> file_contents;
-    while (std::getline(file, str)) {
+    while (file.good()) {
+        std::getline(file, str);
         file_contents.push_back(str);
     }
     return file_contents;
