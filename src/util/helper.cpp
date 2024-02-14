18c2fe1b8c26bd9915644db45abb2ea6e8935b2c
btnguyenPersonal <peteraxefan@gmail.com>
2024-02-14 07:14:07 -0600

    fix allowlist againg

--- src/util/helper.cpp
+++ src/util/helper.cpp
@@ -532,7 +532,7 @@ bool filePathContainsSubstring(const std::filesystem::path& filePath, const std:
 
 bool shouldIgnoreFile(const std::filesystem::path& path) {
     std::vector<std::string> ignoreList = {".git", "node_modules", "build", "dist", "cdk.out", ".next", "tmp"};
-    std::vector<std::string> allowList = {"[...nextAuth]"};
+    std::vector<std::string> allowList = {"[...nextauth]"};
     for (unsigned int i = 0; i < allowList.size(); i++) {
         if (path.string().find(allowList[i]) != std::string::npos) {
             return false;
