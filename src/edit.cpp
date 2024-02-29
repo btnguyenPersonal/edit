60375a0c329678eed90f10d9e1058908b627336a
btnguyenPersonal <peteraxefan@gmail.com>
2024-02-28 20:06:19 -0600

    adding -c for profiling

--- src/edit.cpp
+++ src/edit.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstring>
 #include <ncurses.h>
 #include "util/render.h"
 #include "util/state.h"
@@ -8,11 +9,33 @@
 int main(int argc, char* argv[]) {
     char c;
     State* state;
-    if (argc < 2) {
+    if (argc >= 2 && strcmp(argv[1], "-c") == 0) {
+        if (argc == 2) {
+            std::cerr << "Usage: editor -c '<sequence_of_letters>'\n";
+            return 1;
+        }
+        state = new State();
+        for (int i = 2; i < argc; ++i) {
+            const std::string& sequence = argv[i];
+            char ch;
+            for (size_t j = 0; j < sequence.size(); ++j) {
+                if (sequence[j] == '<' && j + 2 < sequence.size() && sequence[j + 1] == 'c' && sequence[j + 2] == '-') {
+                    char control_char = sequence[j + 3];
+                    ch = ctrl(control_char);
+                    j += 4;
+                } else {
+                    ch = sequence[j];
+                }
+                sendKeys(state, ch);
+            }
+        }
+        endwin();
+        return 0;
+    } else if (argc > 1) {
+        state = new State(argv[1]);
+    } else {
         state = new State();
         generateFindFileOutput(state);
-    } else {
-        state = new State(argv[1]);
     }
     initTerminal();
     calcWindowBounds();
