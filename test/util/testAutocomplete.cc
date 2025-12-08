b70f4483acde7ce4d2216cb1b15aa807bf03c5aa
Ben Nguyen <peteraxefan@gmail.com>
2025-12-08 09:45:49 -0600

    autocomplete tests

--- test/util/testAutocomplete.cc
+++ test/util/testAutocomplete.cc
@@ -36,7 +36,47 @@ struct testSuiteRun testAutocomplete()
 
 	{
 		State *state = new State("./test-file.h", { "", "abc", "acb", "acb" });
-		output.push_back({ "autocomplete on match that is not the first, chooses the most common match", compare(autocomplete(state, "a"), std::string("dddd")) });
+		output.push_back({ "autocomplete on match that is the same as the current string, returns empty string", compare(autocomplete(state, "acb"), std::string("")) });
+	}
+
+	{
+		State *state = new State("./test-file.h", { "", "                    abc                                ", "", "" });
+		output.push_back({ "autocomplete string that is in middle of line", compare(autocomplete(state, "a"), std::string("bc")) });
+	}
+
+	{
+		State *state = new State("./test-file.h", { "                    abc                                ", "                    abc                                ", "", "" });
+		output.push_back({ "autocomplete string that is on the current line", compare(autocomplete(state, "a"), std::string("bc")) });
+	}
+
+	{
+		State *state = new State("./test-file.h", { "abc", "abcdefghijklmnopqrstuvwxyz", "", "" });
+		output.push_back({ "autocomplete long string currently typing", compare(autocomplete(state, "abc"), std::string("defghijklmnopqrstuvwxyz")) });
+	}
+
+	{
+		State *state = new State("./test-file.h", { "abcdefghijklmnopqrstuvwxy", "abcdefghijklmnopqrstuvwxyz", "", "" });
+		output.push_back({ "autocomplete long string almost done typing", compare(autocomplete(state, "abcdefghijklmnopqrstuvwxy"), std::string("z")) });
+	}
+
+	{
+		State *state = new State("./test-file.h", { "a_", "a_b_c_d", "", "" });
+		output.push_back({ "autocomplete with underscores", compare(autocomplete(state, "a_"), std::string("b_c_d")) });
+	}
+
+	{
+		State *state = new State("./test-file.h", { "a(", "a(b(c(d", "", "" });
+		output.push_back({ "autocomplete doesn't work with (", compare(autocomplete(state, "a("), std::string("")) });
+	}
+
+	{
+		State *state = new State("./test-file.h", { "fun(", "function2()", "function()", "function()", "function2()" });
+		output.push_back({ "autocomplete picks the smaller one", compare(autocomplete(state, "fun"), std::string("ction")) });
+	}
+
+	{
+		State *state = new State("./test-file.h", { "function(", "function2()", "function()", "function()", "function2()" });
+		output.push_back({ "autocomplete picks the smaller non-current one", compare(autocomplete(state, "function"), std::string("2")) });
 	}
 
 	return { "test/util/testAutocomplete.cc", output };
