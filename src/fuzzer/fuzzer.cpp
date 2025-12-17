810e4ef400c883ba46a6f8e32240415ca9cfcbc7
Ben Nguyen <peteraxefan@gmail.com>
2025-12-17 08:22:17 -0600

    more fuzzing

--- src/fuzzer/fuzzer.cpp
+++ src/fuzzer/fuzzer.cpp
@@ -16,13 +16,11 @@ void fuzzSendKeys(int testnum, int iterations = 1000)
 	std::vector<char> keypresses = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ' ', '\b', '\n', '\t', '\x00', '\x01', '\x02', '\x03', '\x04', '\x05', '\x06', '\x07', '\x08', '\x09', '\x0A', '\x0B', '\x0C', '\x0D', '\x0E', '\x0F', '\x10', '\x11', '\x12', '\x13', '\x14', '\x15', '\x16', '\x17', '\x18', '\x19', '\x1A', '\x1B', '\x1C', '\x1D', '\x1E', '\x1F', '\x7F', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '\'', '"', '`', '~', ';', ':', '[', ']', '{', '}', ',', '<', '.', '>', '/', '?', '|', '\\', '-', '_', '+', '=' };
 
 	std::uniform_int_distribution<> dis(0, keypresses.size() - 1);
-	system("cp -v src/util/helper.cpp longtest.js");
-	State *state = new State("longtest.js");
+	State *state = new State();
 
 	std::string ret = "{";
 
 	try {
-		state->mode = NORMAL;
 		std::vector<char> randVec;
 		for (int i = 0; i < iterations; i++) {
 			randVec.push_back(keypresses[dis(gen)]);
