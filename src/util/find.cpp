2280c7c70786bec0b4f5bdbff4d687ac9cdd553c
Ben Nguyen <peteraxefan@gmail.com>
2026-01-16 18:14:00 -0600

    fixed scoring of find

--- src/util/find.cpp
+++ src/util/find.cpp
@@ -24,24 +24,78 @@ int32_t maxConsecutiveMatch(const std::filesystem::path &filePath, const std::st
 		return s;
 	};
 
-	std::string fileLower = filePath.string();
-	std::string queryLower = query;
-	if (isAllLowercase(query)) {
-		fileLower = to_lower(fileLower);
-		queryLower = to_lower(queryLower);
+	std::string fileStr = filePath.string();
+	std::string fileLower = to_lower(fileStr);
+	std::string queryLower = to_lower(query);
+
+	auto isWordBoundary = [&](const std::string &str, size_t pos) {
+		if (pos == 0)
+			return true;
+		if (pos >= str.length())
+			return true;
+		char prev = str[pos - 1];
+		char curr = str[pos];
+		return (std::isalnum(prev) && !std::isalnum(curr)) || (!std::isalnum(prev) && std::isalnum(curr));
+	};
+
+	int32_t caseSensitiveScore = 0;
+	size_t filePos = 0;
+	bool caseSensitiveMatch = true;
+
+	for (char queryChar : query) {
+		size_t foundPos = fileStr.find(queryChar, filePos);
+		if (foundPos == std::string::npos) {
+			caseSensitiveMatch = false;
+			break;
+		}
+
+		int32_t points = 2;
+		if (foundPos == filePos) {
+			points += 2;
+		}
+		if (isWordBoundary(fileStr, foundPos)) {
+			points += 3;
+		}
+
+		caseSensitiveScore += points;
+		filePos = foundPos + 1;
+	}
+
+	if (caseSensitiveMatch) {
+		if (fileStr.find(query) != std::string::npos) {
+			caseSensitiveScore += query.size() * 3;
+		}
+		caseSensitiveScore += fileStr.length() / 10;
+		return caseSensitiveScore;
+	}
+
+	int32_t caseInsensitiveScore = 0;
+	filePos = 0;
+
+	for (char queryChar : queryLower) {
+		size_t foundPos = fileLower.find(queryChar, filePos);
+		if (foundPos == std::string::npos) {
+			return 0;
+		}
+
+		int32_t points = 1;
+		if (foundPos == filePos) {
+			points += 1;
+		}
+		if (isWordBoundary(fileLower, foundPos)) {
+			points += 2;
+		}
+
+		caseInsensitiveScore += points;
+		filePos = foundPos + 1;
 	}
 
 	if (fileLower.find(queryLower) != std::string::npos) {
-		return query.size();
+		caseInsensitiveScore += queryLower.size() * 2;
 	}
+	caseInsensitiveScore += fileStr.length() / 20;
 
-	for (size_t len = queryLower.size() - 1; len > 0; len--) {
-		if (fileLower.find(queryLower.substr(0, len)) != std::string::npos) {
-			return static_cast<int32_t>(len);
-		}
-	}
-
-	return 0;
+	return caseInsensitiveScore;
 }
 
 std::vector<std::filesystem::path> find(const std::filesystem::path &dir_path, const std::string &query)
@@ -81,8 +135,13 @@ std::vector<std::filesystem::path> find(const std::filesystem::path &dir_path, c
 		}
 	}
 	std::sort(matching_files.begin(), matching_files.end(), [&](const std::filesystem::path &a, const std::filesystem::path &b) {
-		int32_t matchA = cache[a];
-		int32_t matchB = cache[b];
+		auto itA = cache.find(a);
+		auto itB = cache.find(b);
+		if (itA == cache.end() || itB == cache.end()) {
+			return a.string() < b.string();
+		}
+		int32_t matchA = itA->second;
+		int32_t matchB = itB->second;
 		if (matchA == matchB) {
 			return a.string() < b.string();
 		}
